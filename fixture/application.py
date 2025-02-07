ea37b6c39edcf3008a8df9f99c43e1d8ec42f8b4
aekus21 <aekus21@gmail.com>
2025-02-07 12:29:02 +0300

    В файлах add_contact.py и application.py прописаны связи с вспомогательным классом ContactHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-
+from fixture.contact import ContactHelper
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 
@@ -11,84 +10,11 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
-# Функции для add_test_group_case2.py
     def open_homepage(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
-
-# Функции для add_contact.py
-    def new_contact_form(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def fill_form(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("photo").send_keys(contact.photo)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birth_day)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birth_month)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.birth_year)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.anniversary_day)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.anniversary_month)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
-        wd.find_element_by_name("new_group").click()
-        Select(wd.find_element_by_name("new_group")).select_by_visible_text(contact.select_group)
-        wd.find_element_by_xpath("//input[20]").click()
-        self.open_home()
-
-    def open_home(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home").click()
-
--- test/add_contact.py
+++ test/add_contact.py
@@ -11,13 +11,13 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login_form("admin", "secret")
-    app.new_contact_form()
-    app.fill_form(Contact('Svyatoslav', '__test', 'Ivanov',
+    app.contact.new_contact_form()
+    app.contact.fill_form(Contact('Svyatoslav', '__test', 'Ivanov',
                                    'aekus_test', 'D:\python_traning\image.jpg',
                                    'new_title', 'new_comp',
                                    'new_test_addr', 'test_home', '123546',
                                    'new_test_work', '784512', 'new_test_email1',
                                    'email2', '', 'new_test_homepage',
                                    '16', 'December', '1880',
-                                   '10', 'May', '1650', 'test_group'))
+                                   '10', 'May', '1650', 'test_group2'))
     app.session.logout()
