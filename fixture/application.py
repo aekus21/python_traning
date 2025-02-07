1397286f53452c5826461ccb9dfc7560dfa86819
aekus21 <aekus21@gmail.com>
2025-02-07 23:32:54 +0300

    добавлены помощники EditContactHelper и EditGroupHelper добавлен метод удаления первой группы

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from fixture.contact import ContactHelper
+from fixture.edit_contact import EditContactHelper
+from fixture.edit_group import EditGroupHelper
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 
@@ -11,6 +13,8 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.edit_contact = EditContactHelper(self)
+        self.edit_group = EditGroupHelper(self)
 
     def open_homepage(self):
         wd = self.wd
