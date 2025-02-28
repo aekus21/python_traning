9a87bf5ab54523bfd1216216b0de66a291019589
aekus21 <aekus21@gmail.com>
2025-02-28 14:52:23 +0300

    Задание 11.2 выполнено сравнение списков при удалении контакта

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -10,4 +10,9 @@ def test_delete_first_contact(app):
                                    'email2', '', 'new_test_homepage',
                                    '16', 'December', '1880',
                                    '10', 'May', '1650'))
+    old_contacts_list = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contact_list = app.contact.get_contact_list()
+    assert len(old_contacts_list) - 1 == len(new_contact_list)
+    old_contacts_list[0:1] = []
+    assert old_contacts_list == new_contact_list
