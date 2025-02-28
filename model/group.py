ce717e707660c03816b5208d9af6a1a00e727dad
aekus21 <aekus21@gmail.com>
2025-02-28 10:29:56 +0300

    добавлено сравнение списков для групп

--- model/group.py
+++ model/group.py
@@ -1,5 +1,6 @@
 class Group:
-    def __init__(self, name = None, header = None, footer = None):
+    def __init__(self, name = None, header = None, footer = None, id = None):
         self.test_name = name
         self.test_header = header
         self.test_footer = footer
+        self.id = id
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,7 +2,13 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group("test_group2", "test_header_2", "test_footer_2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group("", "", ""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
