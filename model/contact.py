7affee95bd18bf011dbadfdf6601660af7da2028
aekus21 <aekus21@gmail.com>
2025-03-21 17:38:13 +0300

    добавлна файл orm.py, настроены соответствующие привязки

--- model/contact.py
+++ model/contact.py
@@ -1,11 +1,12 @@
 from sys import maxsize
 
 class Contact:
-    def __init__(self, fname = None, mname = None, lname =None, nickname = None, photo = None,
+    def __init__(self, id = None, fname = None, mname = None, lname =None, nickname = None, photo = None,
                  title = None, company = None, address = None, homephone = None, mobilephone = None,
                  workphone = None, fax = None, email1 = None, email2 = None, email3 = None, homepage = None,
                  bday = None, bmonth = None, byear = None, anniverday = None, annivermonth = None,
                  anniveryear = None, group = None, ids = None, all_phones_from_homepage = None, all_email = None):
+        self.id = id
         self.first_name = fname
         self.middle_name = mname
         self.last_name = lname
