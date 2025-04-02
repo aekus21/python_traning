5b9ecbc3abfd815dd02a168d8666729b45ac3740
aekus21 <aekus21@gmail.com>
2025-04-02 12:17:42 +0300

    добавлен тест сравнения UI и БД контактов

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,4 +1,5 @@
 from model.group import Group
+from model.contact import Contact
 import re
 
 def test_group_list(app, db):
@@ -8,3 +9,12 @@ def test_group_list(app, db):
         return Group(ids=group.id, name=s.strip())
     db_list = map(clean, filter(lambda x: x is not None, (db.get_group_list())))
     assert sorted(ui_list, key= Group.id_or_max) == sorted(db_list, key= Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        s = re.sub('  ', ' ', contact.last_name)
+        return Contact(ids=contact.id, lname=s.strip())
+    db_list = map(clean, (filter(lambda x: x is not None, (db.get_contact_list()))))
+    assert sorted(ui_list, key= Contact.id_or_max) == sorted(db_list, key= Contact.id_or_max)
