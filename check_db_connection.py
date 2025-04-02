46173b6ce6133e5039a754e6ded923e2d8581c9a
aekus21 <aekus21@gmail.com>
2025-04-02 10:39:52 +0300

    добавлен метод выборки контактов входящих в группу (выполнение дает ошибку)

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,9 +1,10 @@
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host = '127.0.0.1', database = 'addressbook', user = 'root', password='')
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contact_in_group(Group(ids='282'))
     for item in l:
         print(item)
     print(len(l))
