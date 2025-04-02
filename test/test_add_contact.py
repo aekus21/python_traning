b9f58d02c773b51391f155c7e0f94cd101ab6b1f
aekus21 <aekus21@gmail.com>
2025-04-02 11:11:49 +0300

    добавлено сравнение с БД при создании контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,11 +2,11 @@
 from model.contact import Contact
 
 
-def test_add_contact_wo_group(app, json_contacts):
+def test_add_contact_wo_group(app, db, json_contacts):
     contact = json_contacts
-    old_contacts_list = app.contact.get_contact_list()
+    old_contacts_list = db.get_contact_list()
     app.contact.save_contact_wo_group(contact)
-    assert len(old_contacts_list) + 1 == app.contact.count()
-    new_contact_list = app.contact.get_contact_list()
+    # assert len(old_contacts_list) + 1 == app.contact.count()
+    new_contact_list = db.get_contact_list()
     old_contacts_list.append(contact)
     assert sorted(old_contacts_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
