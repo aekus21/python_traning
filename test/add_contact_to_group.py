8f352be6c255daa48b67a335b7ba8bd35e850cc3
aekus21 <aekus21@gmail.com>
2025-04-03 14:22:22 +0300

    почти реализованный тест удаления контакта из группы

--- test/add_contact_to_group.py
+++ test/add_contact_to_group.py
@@ -21,31 +21,19 @@ def test_add_contact_to_group(app, db):
 
 def test_del_contact_from_group(app, db):
     try:
-        groups = db_connect.get_group_list()
-        group = random.choice(groups)
-        contact_in_group = db_connect.get_contacts_in_group(group)
-        if len(contact_in_group) == 0:
-            all_users = db_connect.get_contact_list()
-            contact = random.choice(all_users)
-            app.contact.add_contact_to_group(contact.id, group.id)
-        contacts_in_group = db_connect.get_contacts_in_group(group)
-        contact_to_del = random.choice(contacts_in_group)
-        v = contact_to_del.id
-
-
-        group_list = db_connect.get_group_list()
+        group_list = db.get_group_list()
         group = random.choice(group_list)
+        group_id = group.id
         contact_in_group = db_connect.get_contacts_in_group(group)
-        v = random.choice(contact_in_group)
-        ide = v.id
-        print(contact_in_group)
-        # l = db_connect.get_contacts_in_group(Group(ids=group.id))
+        contact = random.choice(contact_in_group)
+        cont_id = contact.id
         app.open_homepage()
         app.wd.find_element_by_name('group').click()
-        app.wd.find_element_by_xpath("//option[@value='%s']" % g)
-        v = select.find_element_by_tag_name('option').value_of_css_property('g')
-        # v = Select(app.wd.find_element_by_name('group').get_attribute('value'))
-        print(v)
+        app.wd.find_element_by_xpath("//option[@value='%s']" % group_id).click()
+        app.contact.select_contact_by_id(cont_id)
+        app.wd.find_element_by_name('remove').click()
+        app.open_homepage()
+
     finally:
         pass
 
