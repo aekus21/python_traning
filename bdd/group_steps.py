ec45da6cd0f120cf79695f20465b4fa05972ca88
aekus21 <aekus21@gmail.com>
2025-04-06 13:12:31 +0300

    правки по bdd

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -2,19 +2,19 @@ from pytest_bdd import given, when, then
 from model.group import Group
 
 
-@given('a group list', target_fixture='a group list')
+@given('a group list', target_fixture='group_list')
 def group_list(db):
     return db.get_group_list()
 
-@given('a new group with {name}, {header} and {footer}', target_fixture='a new group with <name>, <header> and <footer>')
+@given('a new group with {name}, {header} and {footer}', target_fixture='new_group')
 def new_group(name, header, footer):
     return Group(name=name, header=header, footer=footer)
 
-@when('I add the group to the list')
+@when('I add the group to the list', target_fixture='add_new_group')
 def add_new_group(app, new_group):
     app.group.create(new_group)
 
-@then('the new group list is equal to the old list with added group')
+@then('the new group list is equal to the old list with added group', target_fixture='verify_group_added')
 def verify_group_added(db, group_list, new_group):
     old_groups = group_list
     new_groups = db.get_group_list()
--- bdd/groups.feature
+++ bdd/groups.feature
@@ -1,10 +1,12 @@
-Scenario Outline: Add new group
-  Given a group list
-  Given a new group with <name>, <header> and <footer>
-  When I add the group to the list
-  Then the new group list is equal to the old list with added group
+Feature: Group tests
 
-  Examples:
-  | name | header | footer |
-  | name1 | header1 | footer1 |
-  | name2 | header2 | footer2 |
+  Scenario Outline: Add new group
+      Given a group list
+      Given a new group with <name>, <header> and <footer>
+      When I add the group to the list
+      Then the new group list is equal to the old list with added group
+
+      Examples:
+      | name | header | footer |
+      | name1 | header1 | footer1 |
+      | name2 | header2 | footer2 |
