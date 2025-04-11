006f24cdcddb4b853d16bf9284f3064238694a8c
aekus21 <aekus21@gmail.com>
2025-04-11 10:00:32 +0300

    еще одна проверка при добавлении контакта в группу

--- test/add_contact_to_group.py
+++ test/add_contact_to_group.py
@@ -21,6 +21,16 @@ def test_add_contact_to_group(app, db):
     group_list = db.get_group_list()
     group = random.choice(group_list)
     contact_wo_group = db.get_contacts_not_in_group(group)
+    if len(contact_wo_group) == 0:
+        app.contact.save_contact_wo_group(Contact('Svyatoslav_wo_new', '_test', 'Ivanov',
+                                                  'aekus_test', '',
+                                                  'new_title', 'new_comp',
+                                                  'new_test_addr', 'test_home', '123546',
+                                                  'new_test_work', '784512', 'new_test_email1',
+                                                  'email2', '', 'email3',
+                                                  '1', 'December', '2222',
+                                                  '1', 'December', '3333'))
+    contact_wo_group = db.get_contacts_not_in_group(group)
     contact = random.choice(contact_wo_group)
     app.contact.add_contact_to_group(contact.id, group.id)
 
